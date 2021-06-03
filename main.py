f67f2db44b4ce034b9d771a49c4217033af950d1
Kartik Budholiya <kartik31193@gmail.com>
2021-06-03 10:00:14 +0530

    new added classes

--- main.py
+++ main.py
@@ -1,114 +1,25 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-# TODO 1: print resource report
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 
-def report():
-    print(resources)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-# TODO 2: check resource sufficient or not
-def res_sufficiency(s):
-    print("so you want: "+s)
-    c=0
-    for r in resources:
-        if r in MENU[s]["ingredients"]:
-            if resources[r]>=MENU[s]["ingredients"][r]:
-                c+=1
-        else:
-            c+=1
-
-    if c==3:
-        cost=MENU[s]["cost"]
-        print("your total is: "+str(cost)+"\n please insert coins." )
-        print("quarters ?")
-        q=int(input())
-        print("dimes ?")
-        d = int(input())
-        print("nickles ?")
-        n = int(input())
-        print("pennies ?")
-        p = int(input())
-        total = process_coins(q, d, n, p)
-        success(int(cost), total, s)
-    else:
-        print("Sorry we don't have that now")
-
-
-
-
-# TODO 3: process coins
-
-def process_coins(q,d,n,p):
-    total=0.25*q+0.1*d+0.05*n+0.01*p
-    return total
-
-
-
-# TODO 4: check transaction successful or not
-
-
-def success(cost,total,s):
-    if total == cost:
-        print("success...here is your order")
-        make_coffee(s,profit,cost)
-
-    if total<cost:
-        print("Sorry that's not enough money. Money refunded.")
-
-    if total>cost:
-        print("success...here is your order and")
-        print("Here is "+str(total-cost)+" dollars in change")
-        make_coffee(s,profit,cost)
-
-# TODO 5: make coffee
-def make_coffee(s,profit,total):
-    for r in resources:
-        if r in MENU[s]["ingredients"]:
-            resources[r] -= MENU[s]["ingredients"][r]
-    profit+=total
-    print("resources left: ")
-    report()
-    print(f"profit earned : ${profit}")
-
-# TODO :main
 is_on = True
 
 while is_on:
-    print("what do you like(espresso/latte/cappuccino): ")
-    like = input()
-    if like=="off":
-        is_on=False
-    if like=="report":
-        report()
-    if like=="espresso" or like=="latte" or like=="cappuccino":
-        res_sufficiency(like)
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
